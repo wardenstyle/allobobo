98701615e8f03be0022fa8b7ed3f2d3bf0c11f81
wardenstyle <80477473+wardenstyle@users.noreply.github.com>
2024-06-11 14:23:18 +0200

    internval de 30 jours

--- ApiRendezvous.php
+++ ApiRendezvous.php
@@ -24,7 +24,8 @@
 
             include('allobobo_bdd.php');
             $stmt = $bdd->prepare( "SELECT id, jour, nom, email, nom_medecin FROM rdv 
-            INNER JOIN medecin ON medecin.id_medecin = rdv.id_medecin WHERE email = :email");
+            INNER JOIN medecin ON medecin.id_medecin = rdv.id_medecin WHERE email = :email AND 
+            jour BETWEEN NOW()- INTERVAL 30 DAY AND NOW()+ INTERVAL 30 DAY");
 
             if($stmt->execute([':email' => $email])) {
                 return $stmt;
