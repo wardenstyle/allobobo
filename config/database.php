e0b68f548a67c719c9241238baf70d730d4f5ea0
wardenstyle <david.pouille87@gmail.com>
2024-07-08 16:38:54 +0200

    implementation des fonctions de la bdd avec prefixe

---
+++ config/database.php
@@ -0,0 +1,96 @@
+<?php 
+
+class wd_allobobo_db {
+
+    public $prefix = '';
+
+    // Attention à l'ordre des tables à cause des contraintes, classer dans l'ordre pour la suppression des tables
+    // Prendre l'ordre inverser de la création des contraintes
+    public $tables = array(	
+
+        'rendez_vous',		
+        'utilisateur',
+        'medecin',
+        'service_status',
+
+    );
+
+    // Les champs
+    private $init_fields;
+    private $fields;
+
+    // Les noms des tables
+    public $rendez_vous;
+    public $utilisateur; 
+    public $medecin; 
+    public $service_status;
+
+
+    public function __construct( ) {
+        
+        $this->set_prefix("ab_");
+        require_once( 'database_fields.php' );
+    }
+
+    public function set_prefix( $prefix) {
+
+        if ( preg_match( '|[^a-z0-9_]|i', $prefix ) ) {
+            return throw new InvalidArgumentException('Le préfixe doit être une chaîne de caractères valide.');
+        }
+
+        $old_prefix = $prefix;
+
+        $this->prefix = $prefix;
+
+        foreach ( $this->tables() as $table => $prefixed_table ) {
+            $this->$table = $prefixed_table;
+        }
+
+        // Réinit
+        $this->fields =  null;
+        
+        return $old_prefix;
+    }
+
+    public function tables( $with_prefix = true ) {
+        $tables = $this->tables;
+
+        if ( $with_prefix ) {
+            $prefix   = $this->prefix;
+            foreach ( $tables as $k => $table ) {
+                $tables[ $table ] = $prefix . $table;
+                unset( $tables[ $k ] );
+            }
+        }
+
+        return $tables;
+    }
+
+    public function fields(){
+        // Calculer que si c'est non initialisé
+        if (!isset($this->fields)) {
+            $tables = $this->tables();
+            $default_values = 
+                array(
+                    'autoinc' => false,
+                    'key' => false,
+                    'type' => 'string',
+                    'mandatory' => true,
+                    'in_select' => true,
+                    'caption' => false,
+                    'update' => true,
+                    );
+                    
+            foreach($this->init_fields as $t => $cols)
+            {
+                foreach($cols as $col => $values) {
+                    // On utilise le nom de la vrai table comme key
+                    // On merge la valeur défaut
+                    $this->fields[$tables[$t]][$col] = array_merge($default_values, $values);
+                }
+            }
+        }
+        return $this->fields;
+    }
+
+}
---
+++ config/schema.php
@@ -0,0 +1,73 @@
+<?php
+
+function pl_get_db_schema() {
+    global $ab_db;
+
+    /**
+     * The database character collate.
+     */
+    $charset_collate = get_charset_collate();
+
+    public function get_charset_collate() {
+        $charset_collate = '';
+    
+        if ( ! empty( $this->charset ) ) {
+            $charset_collate = "DEFAULT CHARACTER SET $this->charset";
+        }
+        if ( ! empty( $this->collate ) ) {
+            $charset_collate .= " COLLATE $this->collate";
+        }
+    
+        return $charset_collate;
+    }
+
+    // Blog-specific tables.
+$ab_tables = "
+
+CREATE TABLE $ab_db->rendez_vous (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    jour datetime NOT NULL,
+    id_medecin INT NOT NULL,
+    nom VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    annulation VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id)
+)
+$charset_collate;	
+    
+CREATE TABLE $ab_db->utilisateur (
+    code INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nom_user VARCHAR(50) NOT NULL,
+    email_user VARCHAR(50) NOT NULL,
+    mdp VARCHAR(100) NOT NULL,
+    type_compte VARCHAR(5) DEFAULT NULL,
+    id_rdv INT DEFAULT NULL,
+    PRIMARY KEY (code)
+)
+$charset_collate;	
+    
+CREATE TABLE $ab_db->medecin (
+    id_medecin INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nom_medecin VARCHAR(50) NOT NULL,
+    email_medecin VARCHAR(50) NOT NULL,
+    disponibilite VARCHAR(50) NOT NULL,
+    specialite VARCHAR(50) NOT NULL,
+    image VARCHAR(100) NOT NULL,
+    code_user INT NOT NULL,
+    PRIMARY KEY (id_medecin)
+)
+$charset_collate;
+
+
+CREATE TABLE $ab_db->service_status (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    status ENUM('active', 'inactive') NOT NULL,
+    PRIMARY KEY(id)
+
+)
+$charset_collate;
+  
+";
+
+    return $ab_tables;
+}
