0bcc07b576ef3a5a1429c3e6808c9010672cad48
wardenstyle <david.pouille87@gmail.com>
2024-07-10 13:40:39 +0200

    Maj Readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Bou !
 Bou ! la mise à jour du 11.06.2024 intègration d'un web service
 Initialisation du projet de modernisation
+Maj du 10.07.24 :
+-fonction de création-suppression automatique des tables et des insertions
+-IHM administration
--- database/database_data.php
+++ database/database_data.php
@@ -14,6 +14,11 @@ function ab_get_data() {
     (4, 'admin', 'admin@allobobo.fr','ADM', '21232f297a57a5a743894a0e4a801fc3', null)
     ;
 
+    INSERT INTO $ab_db->medecin (`id_medecin`, `nom_medecin`, `email_medecin`, `disponibilite`,`specialite`,`image`, `code_user`) VALUES
+    (1, 'Dr Queen', 'queen@allobobo.fr', '1','Médecin généraliste','images/m1.png','1'),
+    (2, 'Dr Who', 'who@allobobo.fr', '1','Cardiologue','images/m2.png','2'),
+    (3, 'Dr House', 'franck@allobobo.fr', '1','Anesthésiste','images/m3.png','3');
+
     INSERT INTO $ab_db->service_status (id, status) VALUES (1, 'inactive');
        
     "];
